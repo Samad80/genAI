104ad14f809a88e6b2362d6dd487853cff023057
Samad80 <101449155+Samad80@users.noreply.github.com>
2025-09-20 07:26:51 -0700

    Update pp_sumarizer.py

--- pp_sumarizer.py
+++ pp_sumarizer.py
@@ -3,13 +3,13 @@ import torch
 import streamlit as st
 
 pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0", torch_dtype=torch.bfloat16, device_map="auto")
-st.title("Privacy Policy summarization with TinyLlama")
+st.title("Privacy Policy Assistant")
 
 app_name = st.selectbox("App Name", ["WhatsApp", "Facebook", "Instagram", "Twitter", "Snapchat"])
 
 
-if st.button("Summarize"):
-    with st.spinner("Summarizing..."):
+if st.button("Generate"):
+    with st.spinner("Generating..."):
         messages = [
             {
                 "role": "privacy policy expert",
@@ -22,6 +22,7 @@ if st.button("Summarize"):
             ]
         prompt = pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=False)
         outputs = pipe(prompt, max_new_tokens=256, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
-        st.write("Summary:")
+        st.write("Generated Summary:")
         st.write(outputs[0]["generated_text"])
+
             
